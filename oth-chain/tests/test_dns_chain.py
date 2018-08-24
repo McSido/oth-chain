f2d7b744d4ea6f5065f3b6619b22cbf6361bd99c
daniel.kucko <kucko.daniel@freenet.de>
2018-08-24 12:42:48 +0200

    fixed a bug in dns_chain causing domains to get lost on transfer, fixed the corresponding tests

--- oth-chain/tests/test_dns_chain.py
+++ oth-chain/tests/test_dns_chain.py
@@ -97,6 +97,8 @@ class TestDDos(object):
             self.sender_verify = self.receiver_verify
             self.sender_sign = self.receiver_sign
 
+            self.chain.process_message(('mine', self.sender_verify, 'local'))
+
             t = self.create_transaction(chains.DNS_Data('u',
                                                         'seclab.oth',
                                                         '127.0.0.3'),
